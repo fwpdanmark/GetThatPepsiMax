41cd28e0d30b4bfe924209ae19e96481a3fe8dc9
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2017-12-07 20:40:41 +0100

    HAHAH

--- GetThatPepsiMax/Form1.Designer.cs
+++ GetThatPepsiMax/Form1.Designer.cs
@@ -29,11 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 150;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AccessibleName = "ScoreBoard";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(784, 491);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(800, 530);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- GetThatPepsiMax/Form1.cs
+++ GetThatPepsiMax/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,130 @@ namespace GetThatPepsiMax
 {
     public partial class Form1 : Form
     {
+        private int _x = 0;
+        private int _y=0;
+        private int Width = 40;
+        private int Heigth = 60;
+        private int _score = 0;
+        private Random rand;
+        private Graphics View;
+
+        
+
+        private int pepsix;
+        private int pepsiy;
+        private int pepsiWidth = 20;
+        private int pepsiHeigth = 40;
+        private List<int> pepsCollisonX = new List<int>();
+        private List<int> pepsCollisonY = new List<int>();
+
+        enum Position
+        {
+            Left, Right, Up, Down
+        }
+
+        private Position objPositon;
+
+
         public Form1()
         {
             InitializeComponent();
+            rand = new Random();
+            MakeRandomPos();
+            _x = 150;
+            _y = 150;
+           
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            View = e.Graphics;
+            View.DrawImage(new Bitmap("Jacob1.PNG"), _x,_y, Width,Heigth );
+            label2.Text = _score.ToString();
+            View.DrawImage(new Bitmap("Cola.PNG"), pepsix,pepsiy, pepsiWidth,pepsiHeigth);
+            
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (objPositon == Position.Right)
+            {
+                _x += 15;
+               
+            }
+            if (objPositon == Position.Left)
+            {
+                _x -= 15;
+                
+            }
+            if (objPositon == Position.Up)
+            {
+                _y -= 15;
+                
+            }
+            if (objPositon == Position.Down)
+            {
+                _y += 15;
+            }
+            Collision();
+            Invalidate();
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Right)
+            {
+                objPositon = Position.Right;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                objPositon = Position.Left;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                objPositon = Position.Up;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                objPositon = Position.Down;
+            }
+        }
+
+        private void MakeRandomPos()
+        {
+            pepsix = rand.Next(10, 790);
+            pepsiy = rand.Next(10, 510);
+        }
+
+        private void Collision()
+        {
+            for (int i = pepsix; i <= pepsix + pepsiWidth; i++)
+            {
+                pepsCollisonX.Add(i);
+            }
+            for (int i = pepsiy; i <= pepsiy + pepsiHeigth; i++)
+            {
+                pepsCollisonY.Add(i);
+            }
+
+            if (pepsCollisonX.Contains(_x))
+            {
+                ScoreTick();
+                MakeRandomPos();
+            }
+            if (pepsCollisonY.Contains(_y))
+            {
+                ScoreTick();
+                MakeRandomPos();
+            }
+            pepsCollisonX.Clear();
+            pepsCollisonY.Clear();
+        }
+
+        private void ScoreTick()
+        {
+            _score++;
         }
     }
 }
